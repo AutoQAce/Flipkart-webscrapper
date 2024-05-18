eeac37872f1d8a33483887aabc9b5b6dee38aed1
Bhavya Mandaliya <169665702+AutoQAce@users.noreply.github.com>
2024-05-18 19:05:03 +0800

    Delete basic.py

--- basic.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-search_str="Data Science"
-
-search_url=f"https://www.udemy.com/courses/search/?q={'+'.join(search_str.split(' '))}"
-page = requests.get(search_url)
-with open('searchResults.html', 'wb+') as f:
-    f.write(page.content)
